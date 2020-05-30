e5da984702c899bde0fc7f3ff98eff61617a20ab
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2020-05-31 01:40:25 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+# required
+gem "sinatra"
+gem "sinatra-contrib"
+gem "recaptcha"
+
+# miscellaneous
+gem "rufo"
---
+++ server.rb
@@ -0,0 +1,32 @@
+require "bundler/setup"
+require "sinatra"
+require "sinatra/reloader"
+require "recaptcha"
+
+# The following site_key and secret_key are defined by Google for testing purposes.
+# https://developers.google.com/recaptcha/docs/faq
+Recaptcha.configure do |config|
+  config.site_key = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+  config.secret_key = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+end
+
+include Recaptcha::Adapters::ControllerMethods
+include Recaptcha::Adapters::ViewMethods
+
+get "/" do
+  <<-HTML
+    <h1>Demo for reCAPTCHA V2 automated testing</h1>
+    <form action="/verify">
+      #{recaptcha_tags}
+      <button type="submit">Submit</button>
+    </form>
+HTML
+end
+
+get "/verify" do
+  if verify_recaptcha
+    "success"
+  else
+    "fail"
+  end
+end
